ee76a3e1e4b8f8945924ee1e1b0d999dde74a49d
smolkovkonstantin <konstantin.w.smolkov@gmail.com>
2023-11-02 23:01:49 +0300

    add sum

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var scanner = new Scanner(System.in);
+
+        int a =  scanner.nextInt();
+        int b =  scanner.nextInt();
+
+        System.out.println(a + b);
     }
 }
